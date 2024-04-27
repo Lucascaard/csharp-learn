efb2af00fbeede07e9212ee2da849f7e84a103c2
Lucas Cardoso <adslucascardoso@gmail.com>
2024-04-27 20:31:38 -0300

    conceito de pilhas, usando como exeplo didatico pratos

---
+++ MyApp/PilhaDePratos.cs
@@ -0,0 +1,35 @@
+﻿namespace Acme.Collections;
+
+public class PilhaDePratos<T>
+{
+	Entrada? prato;
+
+	public void NovoPrato(T dados)
+	{
+		prato = new Entrada(prato, dados);
+	}
+
+	public T RetiraPrato()
+	{
+		if (prato == null)
+		{
+			throw new InvalidOperationException();
+		}
+		T result = prato.Dados;
+		prato = prato.proximo;
+
+		return result;
+	}
+
+	class Entrada
+	{
+		public Entrada proximo { get; set; }
+		public T Dados { get; set; }
+
+		public Entrada(Entrada proximo, T dados)
+		{
+			this.proximo = proximo;
+			Dados = dados;
+		}
+	}
+}
