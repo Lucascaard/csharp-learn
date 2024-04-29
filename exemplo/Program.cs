eb4a4f169c61fe9f934354e1b7df68122f551d3c
Lucas Cardoso <adslucascardoso@gmail.com>
2024-04-28 22:04:05 -0300

    incluindo projeto 'exemplo

---
+++ exemplo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+
+namespace main
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pessoa lucas = new()
+            {
+                Nome = "lucas",
+                Idade = 27
+            };
+
+            lucas.Apresentar();
+
+        }
+    }
+}
