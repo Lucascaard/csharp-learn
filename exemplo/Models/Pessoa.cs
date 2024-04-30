0b3d94e73f12bfd414c6f3fe5c15623f10c98b6a
Lucas Cardoso <adslucascardoso@gmail.com>
2024-04-30 13:02:37 -0300

    update

--- exemplo/Models/Pessoa.cs
+++ exemplo/Models/Pessoa.cs
@@ -5,12 +5,9 @@ namespace Models
         public string? Nome { get; set; }
         public int Idade { get; set; }
 
-        public byte Teste { get; set; }
-
-
-        public void Apresentar()
+        public void Apresentar(DateTime data)
         {
-            Console.WriteLine($"Olá meu nome é {Nome}!\nEu eu tenho {Idade} anos!");
+            Console.WriteLine($"Olá meu nome é {Nome}! \nEu eu tenho {Idade} anos! \nData atual + 10 dias: {data.ToString("dd/MM/yyyy HH:mm")}");
         }
     }
 }
--- exemplo/Program.cs
+++ exemplo/Program.cs
@@ -8,12 +8,25 @@ namespace main
         {
             Pessoa lucas = new()
             {
-                Nome = "lucas",
-                Idade = 27,
-                Teste = 1
+                Nome = "Lucas Cardoso",
+                Idade = 27
             };
 
-            lucas.Apresentar();
+            DateTime dateTime = DateTime.Now.AddDays(10);
+
+            //lucas.Apresentar(dateTime);
+
+            double _double = double.MaxValue;
+            long _long = long.MaxValue;
+            float _float = float.MaxValue;
+            int inteiro = int.MaxValue;
+            ulong _ulong = ulong.MaxValue;
+            uint _uint = uint.MaxValue;
+            byte _byte = byte.MaxValue;
+
+            long longo = Convert.ToInt32(inteiro);
+            Console.WriteLine($"Max int: {inteiro} \nMax Double: {_double} \nMax float: {_float} ");
+            Console.WriteLine($"Max long: {_long} \nMax byte: {_byte} \nMax uint: {_uint} \nMax ulong: {_ulong}");
         }
     }
 }
