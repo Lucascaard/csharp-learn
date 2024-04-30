b031be8750560c60437bd352a68ea56e1e71f918
Lucas Cardoso <adslucascardoso@gmail.com>
2024-04-30 15:19:54 -0300

    update

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassLibrary.Models;
+
+namespace Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            People lucas = new()
+            {
+                Nome = "Lucas Cardoso",
+                Idade = 27,
+                Altura = 1.80F,
+                Cidade = "Curitiba"
+            };
+
+            lucas.Apresentar();
+        }
+    }
+}
