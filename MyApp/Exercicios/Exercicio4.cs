5576ad2baec60922f9a5f2ac775a2e5b4bc9fc78
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-04 19:48:38 -0300

    Update Exercicio4.cs

--- MyApp/Exercicios/Exercicio4.cs
+++ MyApp/Exercicios/Exercicio4.cs
@@ -7,6 +7,67 @@ namespace MyApp.Exercicios
 {
     public class Exercicio4
     {
-        
+        /*
+        using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+Console.WriteLine("Concatenação de números!");
+string num1 = "10";
+string num2 = "20";
+string result = num1 + num2;
+Console.WriteLine(result);
+Line();
+
+Console.WriteLine("VALOR DEFINIDO COM CULTURA GLOBAL");
+decimal salario = 1582.40M;
+Console.WriteLine($"{salario:C}");
+Line();
+
+Console.WriteLine("FORMATANDO COM CULTURA ESPECIFICA");
+Console.WriteLine(salario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Line();
+
+Console.WriteLine("OUTROS");
+Console.WriteLine(salario.ToString("C3"));
+Console.WriteLine(salario.ToString("N3"));
+Line();
+
+Console.WriteLine("PORCENTAGEM");
+double percentual = .5269;
+Console.WriteLine(percentual.ToString("P2"));
+Line();
+
+Console.WriteLine("FORMATO PERSONLIZADO");
+int numero = 121255;
+Console.WriteLine(numero.ToString("##/##/##"));
+DateTime data = DateTime.Now;
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+Console.WriteLine(data.ToShortDateString());
+Console.WriteLine(data.ToShortTimeString());  
+
+Console.WriteLine("USANDO TryParse para capturar excessão de formatação invalida");
+
+//Data errada para gerar exception
+
+string data2 = "22/01/2024";
+
+bool sucess = DateTime.TryParseExact(data2, "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out DateTime NovaData);
+
+if (sucess)
+{
+    Console.WriteLine("Conversão bem sucedida! " + NovaData.ToShortDateString());
+}
+else
+{
+    Console.WriteLine("Erro ao converter a data: " + data2);
+}
+void Line()
+{
+    Console.WriteLine();
+}
+*/
     }
 }
