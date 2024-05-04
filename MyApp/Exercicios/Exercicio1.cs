d19cd7e021ea82809cc0d41cfa5918373a913a11
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-04 19:50:31 -0300

    Update Exercicio1.cs

--- MyApp/Exercicios/Exercicio1.cs
+++ MyApp/Exercicios/Exercicio1.cs
@@ -7,6 +7,35 @@ namespace MyApp.Exercicios
 {
     public class Exercicio1
     {
-        
+        /*
+         // EXE 1 
+            Console.WriteLine("Insira o salario bruto: ");
+            string input = Console.ReadLine();
+            double salarioBruto = double.Parse(input);
+
+            Console.WriteLine("Insira os beneficios: ");
+            input = Console.ReadLine();
+            double beneficio = double.Parse(input);
+
+            double aliquota;
+            double salarioLiquido;
+
+            if (salarioBruto <= 1100)
+            {  // 5%
+                aliquota = salarioBruto * 0.05;
+            }
+            else if (salarioBruto <= 2500)
+            {
+                // 10%
+                aliquota = salarioBruto * 0.10;
+            }
+            else
+            {
+                // 15%
+                aliquota = salarioBruto * 0.15;
+            }
+            salarioLiquido = salarioBruto - aliquota + beneficio;
+            Console.WriteLine("Salario liquido é: " + salarioLiquido);
+        */
     }
 }
