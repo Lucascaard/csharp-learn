dd32113a2bfdf6ec775cefd48d2f203fa0f4016e
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-04 19:53:02 -0300

    up

--- MyApp/Exercicios/Exercicio3.cs
+++ MyApp/Exercicios/Exercicio3.cs
@@ -7,6 +7,26 @@ namespace MyApp.Exercicios
 {
     public class Exercicio3
     {
-        
+        /*
+        Curso ads = new();
+
+            People lucas = new("Lucas Cardoso de Carvalho", 27);
+            People julia = new("Jlia Von Meterhorn", 20);
+
+            People geucemar = new()
+            {
+                Nome = "Geucemar Briatore",
+                Idade = 60
+            };
+            ads.Professor = geucemar.Nome;
+            ads.Nome = "Analise e Desenvolvimento de Sistemas";
+            ads.Alunos = [];
+
+            ads.AdicionarAluno(lucas);
+            ads.AdicionarAluno(julia);
+            Console.WriteLine("Curso: " + ads.Nome);
+            Console.WriteLine("Professor: " + ads.Professor);
+            ads.ListarAlunos();
+        */
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,22 +4,7 @@
     {
         public static void Main(string[] args)
         {
-
-            // Lista para armazenar os itens
-            List<string> itens = [];
-
-            for (int i = 1; i <= 3; i++)
-            {
-                itens.Add(Console.ReadLine());
-
-            }
-
-            // Exibe a lista de itens
-            Console.WriteLine("Lista de itens:");
-            foreach (string item in itens)
-            {
-                Console.WriteLine($"- {item}");
-            }
+            
         }
     }
 }
