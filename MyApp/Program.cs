c4846af0458e318640d7b8231aa03e42872c72cc
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-04 22:47:07 -0300

    Update Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,26 +1,15 @@
 using MyApp.Exercicios;
 
-namespace MyApp
-{
-    public class Program
-    {
-         static void Main(string[] args)
-        {
-            //OBJ
-            Exercicio1 exe1 = new();
-            Exercicio2 exe2 = new();
-            Exercicio3 exe3 = new();
-            Exercicio4 exe4 = new();
-            Exercicio5 exe5 = new();
+// OBJ
+// Exercicio1 exe1 = new();
+// Exercicio2 exe2 = new();
+// Exercicio3 exe3 = new();
+// Exercicio4 exe4 = new();
+Exercicio5 exe5 = new();
 
-           // FUNC
-            exe1.Func1();
-            exe2.Func1();
-            exe3.Func1();
-            exe4.Func1();
-            exe5.Func2();
-
-        }
-    }
-}
-
+// FUNC
+// exe1.Func1();
+// exe2.Func1();
+// exe3.Func1();
+// exe4.Func1();
+exe5.Func2();
