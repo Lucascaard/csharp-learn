10175831ac2fae708cbc32c3a6abd4c64de113eb
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-07 11:24:17 -0300

    update

---
+++ MyApp/Exercicios/IFTernario.cs
@@ -0,0 +1,11 @@
+
+namespace MyApp.Exercicios
+{
+    public class IFTernario
+    {
+        public string Func1(int numero)
+        {
+            return numero % 2 == 0 ? "É par" : "É impar";
+        }
+    }
+}
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,5 +3,9 @@ using MyApp.Exercicios;
 
 
 People p1 = new("Lucas Cardoso", 27);
+IFTernario ife = new();
+
+Console.WriteLine(ife.Func1(32));
+
 
 (string nome, int idade) = p1;
