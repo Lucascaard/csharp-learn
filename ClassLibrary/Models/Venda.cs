63ec98bf1f988891f73bb9324b40b759ce87ad84
Lucas Cardoso <adslucascardoso@gmail.com>
2024-05-08 15:31:14 -0300

    update

--- ClassLibrary/Models/Venda.cs
+++ ClassLibrary/Models/Venda.cs
@@ -1,21 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ClassLibrary.Models
 {
     public class Venda
     {
-        public Venda(int id, string produto, decimal preco)
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
         {
             Id = id;
             Produto = produto;
             Preco = preco;
+            DataVenda = dataVenda;
         }
 
-        public int Id { get; set; }
-        public string Produto { get; set; }
-        public decimal Preco { get; set; }
+        // Sobrescrevendo o método ToString
+        public override string ToString()
+        {
+            return $"ID: {Id}, Produto: {Produto}, Preço: {Preco}, Data de Venda: {DataVenda.ToShortDateString()}";
+        }
     }
 }
